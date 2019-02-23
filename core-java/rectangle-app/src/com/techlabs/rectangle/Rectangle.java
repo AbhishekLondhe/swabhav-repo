e9bc8fe8b3155c9108344a515feb89207cd50e6f
Abhijeet <alondhe649@gmail.com>
2019-02-23 11:20:09 +0530

    assignment

--- core-java/rectangle-app/src/com/techlabs/rectangle/Rectangle.java
+++ core-java/rectangle-app/src/com/techlabs/rectangle/Rectangle.java
@@ -1,13 +1,46 @@
 package com.techlabs.rectangle;
 
 public class Rectangle {
-	public int height=0;
-	public int width=0;
-	
-	public int calculateArea()
-	{
-		int area=0;
-		area = height * width; 
-		return area;
+	private int height;
+	private int width;
+	private ColorType color;
+
+	public Rectangle(int height, int width, ColorType color) {
+		this.height = height;
+		this.width = width;
+		this.color = color;
+		System.out.println("rectangle created");
 	}
+
+	public Rectangle(int height, int width) {
+		this.height = height;
+		this.width = width;
+		this.color = ColorType.RED;
+		System.out.println("rectangle created");
+	}
+
+	public void setHeight(int newHeight)
+	{
+		this.height = newHeight;
+	}
+	
+
+	
+	public int getHeight() {
+		return height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public ColorType getColor() {
+		return color;
+	}
+
+	public int calculateArea() {
+		int Area = this.height * this.width;
+		return Area;
+	}
+
 }
--- core-java/rectangle-app/src/com/techlabs/test/RectangleTest.java
+++ core-java/rectangle-app/src/com/techlabs/test/RectangleTest.java
@@ -1,23 +1,31 @@
 package com.techlabs.test;
+
 import com.techlabs.rectangle.*;
 
 public class RectangleTest {
-	public static void main(String args[])
-	{
-		Rectangle big = new Rectangle();
-		Rectangle small =new Rectangle();
-		
-		big.height =-10;
-		big.width =50;
-		
-		small.height=5;
-		small.width=10;
-		
-		System.out.println("height of big rectangle:"+big.height+" width of big rectangle:"+big.width);
-		System.out.println("area of big reactangle:"+big.calculateArea());
-		System.out.println("\nheight of small rectagle:"+small.height+" width of small rectangle:"+small.width);
-		System.out.println("area of small reactangle:"+small.calculateArea());
-		
-		
+	public static void main(String args[]) {
+		Rectangle r1 = new Rectangle(20, 30, ColorType.BLUE);
+		display(r1);
+
+		Rectangle r2 = new Rectangle(40, 55);
+		display(r2);
+
+		Rectangle r3;
+		r3 = r1;
+		System.out.println("r3 height:" + r3.getHeight());
+		System.out.println("r1 height:" + r1.getHeight());
+		r3.setHeight(11);
+		System.out.println(r3.getHeight());
+		System.out.println(r1.getHeight());
+		System.out.println(new Rectangle(10, 20, ColorType.RED).calculateArea());
+		display(new Rectangle(10, 20));
+	}
+
+	public static void display(Rectangle r) {
+		System.out.println("height: " + r.getHeight());
+		System.out.println("width: " + r.getWidth());
+		System.out.println("Color: " + r.getColor());
+		System.out.println("area:" + r.calculateArea());
+		System.out.println("");
 	}
 }
